cb0a3f8e43f0f968680dc7b171fcf3a1f0a793d5
Facundo D <genuinefafa@gmail.com>
2014-08-28 00:16:41 -0300

    moviendo los archivos para que travis funcione como queremos

---
+++ src/test/groovy/MiTest.groovy
@@ -0,0 +1,7 @@
+import groovy.util.GroovyTestCase
+class MiTest extends GroovyTestCase {
+    void testDeAlgunaCosa() {
+        assert 1 == 1
+        assert 2 + 2 == 5 : "Hay un problema, las cuentas ya no funcionan!"
+    }
+}
