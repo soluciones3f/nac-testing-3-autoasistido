9e05e7f5028cce7fe17783c8e454140bda03bee3
Facundo D <genuinefafa@gmail.com>
2014-08-28 00:24:27 -0300

    test arreglado

--- src/test/groovy/MiTest.groovy
+++ src/test/groovy/MiTest.groovy
@@ -2,6 +2,6 @@ import groovy.util.GroovyTestCase
 class MiTest extends GroovyTestCase {
     void testDeAlgunaCosa() {
         assert 1 == 1
-        assert 2 + 2 == 5 : "Hay un problema, las cuentas ya no funcionan!"
+        assert 2 + 2 == 4 : "Hay un problema, las cuentas ya no funcionan!"
     }
 }
